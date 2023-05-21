b0b0892fc93fa10bb61982eba24df52149b4355a
aljorgevi <jormencar@yahoo.com>
2023-05-21 11:37:23 +0100

    Update Avatar.tsx

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -4,6 +4,12 @@ import Image from 'next/image'
 
 export default function Avatar() {
 	return (
-		<Image className='rounded-full' height={30} width={30} alt='avatar' src='/images/avatar.jpg' />
+		<Image
+			className='rounded-full'
+			height={30}
+			width={30}
+			alt='avatar'
+			src='/images/placeholder.jpeg'
+		/>
 	)
 }
