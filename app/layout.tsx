ba8257da92b1b2525c8c25fe4bb1f661737758e6
aljorgevi <jormencar@yahoo.com>
2023-05-21 11:02:56 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import React from 'react'
 import { Nunito } from 'next/font/google'
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
 
 const font = Nunito({
